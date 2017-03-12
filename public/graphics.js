7b4120bc0e6729f20ab80febce509f1b3ec15426
alexreuter <areuter@lsoc.org>
2017-03-12 18:24:35 -0500

    Basic tank networking done

--- public/graphics.js
+++ public/graphics.js
@@ -1,76 +1,12 @@
-// For some reason this is the problem
-// var socket = io.connect("http://localhost:3000");
 
-/* 
-Extensions:
-Make the bullets only have a certain number of bounces
-Figure out bullets out of turret
-Make tank into an object
-Figure out keypress vs keydown, so can turn and shoot and move at the same time
-Figure out lerping
-Figure out how to make this much less janky, different file with mathy drawing functions
-Now have wall coords, need to use the distance equation to see if distance is less than 
-Idea for reactangle collision. Have a function that checks if a corner is inside a box. Then run that on all four corners and detirmine chich one is indise.
-Then you dont let the corner come less that the x value if inside
-*/
-
-
-/*
-Long term:
- -> Make tank an object
- -> Bullet collisions
-*/
-
-/*
-Make tank speed have friction 
-
-*/
-
-/*
-Need to re-write drawing out the board
-Need to re-orgainize code
-Need to make a setup function
-Figure out how to put into spereate files
-*/
+var socket = io();
 
 
 
-
-/*
-Networking Brainstorm
-
- -> How does dying work? You always check if you are dying, 
- if you do you drop out of the room, no problem reduces unnecessary computation if everyone has to
-
- -> NOT NESSECARY FOR MVP Need to send nicknames over the airwaves, tank data also needs health numbers associated with it
-   -> Later on will be used to display nicknames and leaderboards and chat and other random stuff
-   -> Can just send tank data with health and can function as a game, just cant tell who was killed
-
- On receiving a tank packet, just adds it to a multidimensional of tankdata that is displayed
- Bullet data has its own class
-
-What if each person calculates their own bullets, sends them out, so that everyone else just displays the computed values
-How it worked this summer: startbullet packet, each person calculates and displays, now just need to display ball in the right place
-So when one person dies, their bullets are lost too -> Can be fixed later
-
-
-
-
-
-*/
-
-/*TODO
- - Fix tank display back to normal
- - Write code to display other tanks
- - Look at tile drawing code again
-
- */
-
-/*
-Minor fixes just came up with
- - Can only fire once per space press
-
-
+/*KNOWN BUGS:
+ - Timing issue on the bullets makes canvas re-animate in too short a time
+ - Ids for the tanks are pseudo random, need to be changed to be actual ids
+ - 
 */
 
 
@@ -102,13 +38,13 @@ $(document).ready(function()
 	var windowDimens = [30,30*canvas.height/canvas.width];
 
 
-	// var tankscale = 2;
 	// canvas.width/windowDimens[0] is gonna be the pixels per tile
 	// divding by tank width creates conversion factor, so tank is appropriately scaled to tile size
 	var tankscale = (canvas.width/windowDimens[0])/tankImg.width;
 	console.log("tankscale: " + tankscale);
 	var bulletsize, bulletspeed;
 	var importSize = 40;
+	var initialHealth = 10;
 
 	// Will hold x,y,angle,health,nickname, color
 	var otherTanks = [];
@@ -124,6 +60,8 @@ $(document).ready(function()
 	var rightDown = false;
 	var leftDown = false;
 	var reloaded = true;
+
+	var drawingTimer;
 
 	// This weird callback inside callback makes sure the images are loaded before the game starts
 	spriteSheet.onload = function() {
@@ -142,19 +80,43 @@ $(document).ready(function()
 			bulletsize = tileWidth/10;
 
 			// setInterval(draw,50);
-			setInterval(draw,50);
+			// setInterval(draw,50);
+			drawingTimer = setTimeout(draw,50);
 		}
 	};
 	
+// **************************************** NETWORKING FUNCTIONS **************************************************************************
+	socket.on("tank",function(data)
+	{
+		var id = data[4];
+		var found = false;
 
+		for(i=0;i<otherTanks.length;i++)
+		{
+			if(otherTanks[i][4] == id)
+			{
+				found = true;
+				otherTanks[i] = data;
+			}
+		}
+
+		if(!found)
+		{
+			console.log("Someone new has joined");
+			otherTanks.push(data);
+		}
+
+	});
+	// x,y,angle,health,nickname
+	
 
 // ************************** HELPER FUNCTIONS **************************************************************************
 
 	function printBullets()
 	{
-		for(i = 0; i<bullets.length;i++)
+		for(i = 0; i<otherTanks.length;i++)
 		{
-			console.log(bullets[i]);
+			console.log(otherTanks[i]);
 		}
 		console.log("DONE");
 	}
@@ -257,49 +219,42 @@ $(document).ready(function()
 		// GETS RID OF THE LARGE FUNCTION AND SORTA SPLITS STUFF UP NICLEY
 		this.animate = function()
 		{
-			// THE PROBLEM IS THAT WHEN INITIALLY ASSIGN INDEX, NOW WHEN WE DELETE, INDEX IS WRONG
-			//Updates the bullet position
-
-			// I STW(TCHED THE PACEMENT OIF POSITION UPDATING)
-
-			// WELL I WAS USING THE SPLICE METHOD WRONG
-			// THERE GOES FIVE HOURS OF MY TIME
+			var index = bullets.findIndex(findBullet,this.id);
 
 			if(this.x - (bulletsize/2) < 0)
 			{
-				var index = bullets.findIndex(findBullet,this.id);
 				bullets.splice(index,1);
 			}
 			if(this.x + (bulletsize/2) > canvas.width)
 			{
-				var index = bullets.findIndex(findBullet,this.id);
 				bullets.splice(index,1);
 			}
 			if(this.y - (bulletsize/2) < 0)
 			{
-				var index = bullets.findIndex(findBullet,this.id);
 				bullets.splice(index,1);
 			}
 			if(this.y + (bulletsize/2) > canvas.height)
 			{
-				var index = bullets.findIndex(findBullet,this.id);
 				bullets.splice(index,1);
 			}
-			// else
-			// {
-			// // SO THE PROCESSING OCCURING INSIDE THE DRAWING ISNT NEGLIGABLE
-			// // BUT WALL CORRDS IS EMPTY SO WTF
+			else
+			{
+			// SO THE PROCESSING OCCURING INSIDE THE DRAWING ISNT NEGLIGABLE
+			// BUT WALL CORRDS IS EMPTY SO WTF
 
-			// 	for(i=0;i<wallCoords.length;i++)
-			// 	{
-			// 		console.log("WTF");
-			// 	// 	if(distance(this.x,this.y,wallCoords[i][0] + tileWidth/2, wallCoords[i][1] + tileHeight/2) < tileWidth/2)
-			// 	// 	{
-			// 	// 		alert("INSIDE");
-			// 	// 		bullets.splice(index,1);
-			// 	// 	}
-			// 	}
-			// }
+				for(i=0;i<wallCoords.length;i++)
+				{
+				// 	console.log("WTF");
+				// 	console.log(wallCoords);
+					// if(distance(this.x,this.y,wallCoords[i][0] + tileWidth/2, wallCoords[i][1] + tileHeight/2) < tileWidth/2)
+					// {
+					// 	alert("INSIDE");
+					// 	bullets.splice(index,1);
+					// }
+				}
+
+				console.log("dog");
+			}
 
 			this.x = this.x + this.xspeed;
 			this.y = this.y + this.yspeed;
@@ -312,10 +267,13 @@ $(document).ready(function()
 	{
 		this.x = x;
 		this.y = y;
+		this.nickname = Math.random();
 		this.angle = angle;
 		this.speed = 0;
 		this.xspeed = -this.speed*Math.sin((this.angle*Math.PI)/180);
 		this.yspeed = -this.speed*Math.cos((this.angle*Math.PI)/180);
+		this.health = initialHealth;
+
 		this.animate = function()
 		{
 			this.xspeed = -this.speed*Math.sin((this.angle*Math.PI)/180);
@@ -352,6 +310,10 @@ $(document).ready(function()
 			ctx.drawImage(tankImg,-((tankImg.width)*0.5)*tankscale,-((tankImg.height)*0.5)*tankscale,tankImg.width * tankscale, tankImg.height*tankscale);
 			ctx.rotate(this.angle*Math.PI/180);
 			ctx.translate(-this.x,-this.y);		
+
+			// x,y,angle,health,nickname
+			var data = [this.x,this.y,this.angle,this.health,this.nickname];
+			socket.emit("tank", data);
 		}
 	}
 
@@ -432,6 +394,9 @@ $(document).ready(function()
 	function draw()
 	{
 
+		clearTimeout(drawingTimer);
+
+
 		// //Resets the background canvas from what was drawn previously
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -467,8 +432,23 @@ $(document).ready(function()
 			pointRect(wallCoords[i][0],wallCoords[i][1]);
 		}
 
-		printBullets();
+		// DISPLAYING OTHER TANKS
+
+		for(i=0;i<otherTanks.length;i++)
+		{
+			// x,y,angle,health,nickname
+			var currentTank = otherTanks[i];
+			ctx.translate(currentTank[0],currentTank[1]);
+			ctx.rotate(-currentTank[2]*Math.PI/180);
+			ctx.drawImage(tankImg,-((tankImg.width)*0.5)*tankscale,-((tankImg.height)*0.5)*tankscale,tankImg.width * tankscale, tankImg.height*tankscale);
+			ctx.rotate(currentTank[2]*Math.PI/180);
+			ctx.translate(-currentTank[0],-currentTank[1]);
+		}
+
+		// printBullets();
 		// socket.emit("box", data);
+
+		drawingTimer = setTimeout(draw,50);
 
 	}
 
--- server.js
+++ server.js
@@ -1,28 +1,48 @@
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static('public'));
+
+// var server = app.listen(3000);
+// console.log("Server is working")
+
+// var socket = require("socket.io");
+
+// var io = socket(server);
+
 var express = require('express');
 var app = express();
-
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.use(express.static('public'));
-
-var server = app.listen(3000);
-console.log("Server is working")
-
 var socket = require("socket.io");
-
 var io = socket(server);
 
+
 io.sockets.on("connection", newConnection);
+
+// io.sockets.on('disconnect', function () {
+// 	console.log("Someone left");
+
+//   });
+
 
 function newConnection(socket)
 {
 	console.log("New connection: " + socket.id);
+	socket.on("tank", message);
+
+	function message(data)
+	{
+		console.log(data);
+		socket.broadcast.emit("tank", data);
+		// console.log("sent");
+	}
 }
 
-// 	socket.on("msg", message);
 
-// 	function message(data)
-// 	{
-// 		console.log(data);
-// 		socket.broadcast.emit("msg", data);
-// 		// console.log("sent");
-// 	}
-// }
+
+
+
+
